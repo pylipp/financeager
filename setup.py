f7644b2947fbcb0d0a727f9fafca1b0027e16cef
pylipp <philipp.metzner@tum.de>
2016-10-26 12:21:20 +0200

    Use package structure suitable for pip.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="financeager",
+        version="1.0",
+        description="Command line tool to administer finances",
+        url="http://github.com/pylipp/financeager",
+        author="Philipp Metzner",
+        author_email="beth.aleph@yahoo.de",
+        license="GPLv3",
+        #classifiers=[],
+        packages=find_packages(exclude=["test", "doc"]),
+        install_requires=[]
+        )
