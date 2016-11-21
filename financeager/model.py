ec1784588fef48f555076eb5da0ececcf089e0df
pylipp <philipp.metzner@tum.de>
2016-11-21 12:03:26 +0100

    Add comments to Model class.

--- financeager/model.py
+++ financeager/model.py
@@ -10,6 +10,13 @@ class Model(QStandardItemModel):
         super(QStandardItemModel, self).__init__()
 
     def add_entry(self, entry, category="Unspecified"):
+        """Add a Category- or BaseEntry to the model.
+        Category names are unique, i.e. a CategoryEntry is not skipped if one
+        with identical name (case INsensitive) already exists.
+        When adding a BaseEntry, the parent CategoryItem is created if it does
+        not exist. If no category is specified, the BaseEntry is added to the
+        'Unspecified' category. The corresponding sum item is updated.
+        """
         if isinstance(entry, CategoryEntry):
             if entry.name_item.data() not in self.category_entry_names:
                 self.appendRow(entry.items)
@@ -22,13 +29,23 @@ class Model(QStandardItemModel):
                 sum_item.update(entry.value_item)
 
     def category_entry_items(self, item_type):
-        """yield: DataItem"""
+        """Generator iterating over first-level children (CategoryEntries) of
+        the model. `item_type` is one of `CategoryEntry.ITEM_TYPES`.
+
+        raises: KeyError if `item_type` not found.
+        yields: CategoryItem, SumItem
+        """
         col = CategoryEntry.ITEM_TYPES.keys().index(item_type)
         for row in range(self.rowCount()):
             yield self.item(row, col)
 
     def base_entry_items(self, item_type):
-        """yield: DataItem"""
+        """Generator iterating over second-level children (BaseEntries) of
+        the model. `item_type` is one of `BaseEntry.ITEM_TYPES`.
+
+        raises: KeyError if `item_type` not found.
+        yields: NameItem, ValueItem, DateItem
+        """
         col = BaseEntry.ITEM_TYPES.keys().index(item_type)
         for category_item in self.category_entry_items("name"):
             for row in range(category_item.rowCount()):
@@ -36,7 +53,10 @@ class Model(QStandardItemModel):
 
     @property
     def category_entry_names(self):
-        """yield: QString"""
+        """Convenience generator method yielding category names.
+
+        yield: QString
+        """
         item_generator = self.category_entry_items("name")
         while True:
             try:
@@ -45,13 +65,16 @@ class Model(QStandardItemModel):
                 break
 
     def find_category_item(self, category_name):
-        """return: DataItem"""
+        """Find CategoryItem by given `category_name` or return None if not
+        found. The search is case insensitive.
+        """
         for category_item in self.category_entry_items("name"):
             if category_item.data() == QString(category_name.lower()):
                 return category_item
         return None
 
     def category_sum(self, category_name):
+        """Return sum of category named `category_name`."""
         category_item = self.find_category_item(category_name)
         if category_item is not None:
             return category_item.entry.sum_item.data().toFloat()[0]
