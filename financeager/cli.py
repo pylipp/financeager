6f52af6c3ba28ea0dc0393813c8bc1024a5d4264
pylipp <philipp.metzner@tum.de>
2016-12-04 02:38:43 +0100

    Simplify server launch by name server lookup.

--- financeager/cli.py
+++ financeager/cli.py
@@ -1,42 +1,43 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import socket
 import subprocess
 import Pyro4
 import os
 import sys
+import time
 from financeager.period import Period
 from financeager.server import Server
 
-def launch_server():
-    def _get_lock(name, cmd):
-        _get_lock._lock_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
-        try:
-            _get_lock._lock_socket.bind('\0' + name)
-            subprocess.Popen(cmd.split())
-        except socket.error:
-            pass
-    return _get_lock
+Pyro4.config.COMMTIMEOUT = 1.0
 
 class Cli(object):
 
     def __init__(self, cl_kwargs):
         self._cl_kwargs = cl_kwargs
 
-        launch_server()("financeager_ns",
-                "{} -m Pyro4.naming".format(sys.executable))
+        # launch nameserver. Silence errors if already running
+        DEVNULL = open(os.devnull, 'w')
+        subprocess.Popen("{} -m Pyro4.naming".format(sys.executable).split(),
+                stdout=DEVNULL, stderr=subprocess.STDOUT, close_fds=True)
 
         period_name = self._cl_kwargs.get("period")
         if period_name is None:
             period_name = Period.DEFAULT_NAME
         period_name = str(period_name)
         self._server_name = Server.NAME_STUB.format(period_name)
-        server_script_path = os.path.join(
-                os.path.dirname(os.path.abspath(__file__)), "start_server.py")
-        launch_server()(self._server_name,
-                ' '.join([sys.executable, server_script_path, period_name]))
+        name_server = Pyro4.locateNS()
+        # launch starting script if period server is not registered yet
+        # TODO avoid accidental launching if stop requested
+        try:
+            name_server.lookup(self._server_name)
+        except (Pyro4.naming.NamingError) as e:
+            server_script_path = os.path.join(
+                    os.path.dirname(os.path.abspath(__file__)), "start_server.py")
+            subprocess.Popen([sys.executable, server_script_path, period_name])
+            time.sleep(1.1*Pyro4.config.COMMTIMEOUT)
 
     def __call__(self):
         server = Pyro4.Proxy("PYRONAME:{}".format(self._server_name))
+        # TODO pop in server.run()
         command = self._cl_kwargs.pop("command")
         server.run(command, **self._cl_kwargs)
