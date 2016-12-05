af26b643bb749d28d5bab0cf4c04ff06669a6968
pylipp <philipp.metzner@tum.de>
2016-12-05 21:52:37 +0100

    Introduce abstract Server class.

--- financeager/server.py
+++ financeager/server.py
@@ -2,34 +2,58 @@
 from __future__ import unicode_literals
 import os.path
 import xml.etree.ElementTree as ET
+from abc import ABCMeta, abstractmethod, abstractproperty
 import Pyro4
 from financeager.period import Period
 
 CONFIG_DIR = os.path.expanduser("~/.config/financeager")
 
+class Server(object):
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self, period_name=None):
+        self._running = True
+        self._period_filepath = os.path.join(
+                CONFIG_DIR, "{}.{}".format(self._period.name, self._file_suffix))
+        if not os.path.isdir(CONFIG_DIR):
+            os.makedirs(CONFIG_DIR)
+
+    @abstractproperty
+    def _file_suffix(self):
+        pass
+
+    @property
+    def running(self):
+        return self._running
+
+    @abstractmethod
+    def run(self, command, **kwargs):
+        if command == "stop":
+            self._running = False
+        else:
+            command2method = {"add": "add_entry"}
+            getattr(self._period, command2method[command])(**kwargs)
+
 @Pyro4.expose
-class XmlServer(object):
+class XmlServer(Server):
 
     NAME_STUB = "financeager_server.{}"
 
     def __init__(self, period_name=None):
-        self._running = True
         self._period = Period(period_name)
-        self._period_filepath = os.path.join(
-                CONFIG_DIR, "{}.xml".format(self._period.name))
-        if not os.path.isdir(CONFIG_DIR):
-            os.makedirs(CONFIG_DIR)
+        super(XmlServer, self).__init__(period_name)
         self._read_period_from_file()
+
+    @property
+    def _file_suffix(self):
+        return "xml"
 
     def _read_period_from_file(self):
         if os.path.isfile(self._period_filepath):
             xml_tree = ET.parse(self._period_filepath)
             period_element = xml_tree.getroot()
             self._period.create_from_xml(period_element)
-
-    @property
-    def running(self):
-        return self._running
 
     def dump(self):
         period_element = self._period.convert_to_xml()
@@ -42,9 +66,6 @@ class XmlServer(object):
         method of `Period` will be looked up and returned. This method is then
         supposed to be executed in the caller, passing keyword arguments.
         """
-        if command == "stop":
-            self._running = False
-        else:
-            command2method = {"add": "add_entry"}
-            getattr(self._period, command2method[command])(**kwargs)
+        super(XmlServer, self).run(command, **kwargs)
+        if command != "stop":
             self.dump()
