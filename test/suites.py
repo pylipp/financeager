3e6091c74687dc0647665e5c357e1a01d4a0bae4
pylipp <philipp.metzner@tum.de>
2017-03-19 22:55:55 +0100

    Fix relative import.

--- test/suites.py
+++ test/suites.py
@@ -16,7 +16,7 @@ testRunner = unittest.TextTestRunner(verbosity=2)
 
 # import modules, find suite via suite() method and run it
 for module in modules:
-    exec('import {}'.format(module))
+    exec('from . import {}'.format(module))
     try:
         suite = eval('{}.suite()'.format(module))
         testRunner.run(suite)
