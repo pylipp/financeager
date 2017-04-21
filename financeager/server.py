52f86985bcd4d2a1be0a6c082f7711c568abf649
pylipp <philipp.metzner@tum.de>
2017-04-21 21:33:03 +0200

    Remove XmlServer class.

--- financeager/server.py
+++ financeager/server.py
@@ -69,46 +69,6 @@ class Server(object):
             self._response = response
 
 @Pyro4.expose
-class XmlServer(Server):
-    """Server implementation holding a `XmlPeriod` database.
-
-    The class explicitly loads the period at initialization and dumps it if a
-    modifying command is called.
-
-    NOTE: The development of this class is discontinued.
-    """
-
-    def __init__(self, period_name=None):
-        super(XmlServer, self).__init__(period_name)
-        self._period = XmlPeriod(period_name)
-        self._read_period_from_file()
-
-    @staticmethod
-    def name(period_name):
-        return "financeager_xml_server.{}".format(period_name)
-
-    @property
-    def _file_suffix(self):
-        return "xml"
-
-    def _read_period_from_file(self):
-        if os.path.isfile(self._period_filepath):
-            xml_tree = ET.parse(self._period_filepath)
-            period_element = xml_tree.getroot()
-            self._period.create_from_xml(period_element)
-
-    def dump(self):
-        period_element = self._period.convert_to_xml()
-        xml_tree = ET.ElementTree(period_element)
-        xml_tree.write(self._period_filepath, encoding="utf-8",
-                xml_declaration=True)
-
-    def run(self, command, **kwargs):
-        super(XmlServer, self).run(command, **kwargs)
-        if command not in ["stop"]:
-            self.dump()
-
-@Pyro4.expose
 class TinyDbServer(Server):
     """Server implementation holding a `TinyDbPeriod` database.
 
