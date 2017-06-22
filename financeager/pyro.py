34be4f18a7e3f5d070b0f41307d4a7f3acd0a1cb
pylipp <philipp.metzner@tum.de>
2017-06-23 00:11:10 +0200

    Use Pyro configs when starting server.

--- financeager/pyro.py
+++ financeager/pyro.py
@@ -10,6 +10,7 @@ import time
 import Pyro4
 
 from financeager.server import PyroServer
+from .config import CONFIG
 
 
 Pyro4.config.COMMTIMEOUT = 1.0
@@ -20,11 +21,21 @@ def launch_server():
     Launch Pyro4 nameserver. Silence errors if already running.
     Launch PyroServer via starting script if server is not registered yet.
     """
-    with open(os.devnull, 'w') as DEVNULL:
-        subprocess.Popen("{} -m Pyro4.naming".format(sys.executable).split(),
-                stdout=DEVNULL, stderr=subprocess.STDOUT, close_fds=True)
+    pyro_config = CONFIG["SERVICE:PYRO"]
+    hmac_key = pyro_config.get("hmac_key")
 
-    name_server = Pyro4.locateNS()
+    with open(os.devnull, 'w') as DEVNULL:
+        hmac_key_option = "-k {}".format(hmac_key) if hmac_key else ""
+        command = "pyro4-ns -n {} -p {} {}".format(
+                pyro_config["host"],
+                pyro_config["ns_port"],
+                hmac_key_option
+                )
+        print("Starting Pyro nameserver via '{}'".format(command))
+        subprocess.Popen(command.split())
+                # stdout=DEVNULL, stderr=subprocess.STDOUT, close_fds=True)
+
+    name_server = Pyro4.locateNS(hmac_key=hmac_key)
     try:
         name_server.lookup(PyroServer.NAME)
     except (Pyro4.naming.NamingError) as e:
@@ -36,7 +47,12 @@ def launch_server():
 
 
 def proxy():
-    return Pyro4.Proxy("PYRONAME:{}".format(PyroServer.NAME))
+    proxy = Pyro4.Proxy("PYRONAME:{}".format(PyroServer.NAME))
+
+    # see https://stackoverflow.com/questions/34013578/pyro-how-to-use-hmac-key-with-name-server-communicationerror-hmac-key-config
+    proxy._pyroHmacKey = CONFIG.get("SERVICE:PYRO", "hmac_key", fallback=None)
+
+    return proxy
 
 
 CommunicationError = Pyro4.naming.NamingError
