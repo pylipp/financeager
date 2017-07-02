6c6796e3dfad28f88d3578912a76781f194cf105
pylipp <philipp.metzner@tum.de>
2017-07-02 19:09:21 +0200

    Remove XmlPeriod.

--- financeager/period.py
+++ financeager/period.py
@@ -6,13 +6,12 @@ from collections import defaultdict, Counter
 from dateutil import rrule
 from datetime import datetime as dt
 
-import xml.etree.ElementTree as ET
 from tinydb import TinyDB, Query, where, JSONStorage
 from tinydb.database import Element
 from tinydb.queries import QueryImpl
 
 from financeager.model import Model
-from financeager.entries import BaseEntry, CategoryEntry
+from financeager.entries import CategoryEntry
 from financeager.items import DateItem, CategoryItem
 from .config import CONFIG_DIR
 
@@ -27,52 +26,6 @@ class Period(object):
     @property
     def name(self):
         return self._name
-
-class XmlPeriod(Period):
-
-    def __init__(self, name=None, xml_element=None, models=None):
-        super(XmlPeriod, self).__init__(name)
-        # TODO store models in dict
-        self._earnings_model = None
-        self._expenses_model = None
-        if models is not None and len(models) == 2:
-            self._earnings_model, self._expenses_model = models
-        elif xml_element is not None:
-            self.create_from_xml(xml_element)
-        if self._earnings_model is None:
-            self._earnings_model = Model(name="earnings")
-        if self._expenses_model is None:
-            self._expenses_model = Model(name="expenses")
-
-    def create_from_xml(self, xml_element):
-        for model_element in xml_element.findall("model"):
-            name = model_element.get("name")
-            if name == "earnings":
-                self._earnings_model = Model(model_element)
-            elif name == "expenses":
-                self._expenses_model = Model(model_element)
-        self._name = xml_element.get("name", Period.DEFAULT_NAME)
-
-    def convert_to_xml(self):
-        period_element = ET.Element("period", name=self._name)
-        period_element.text = "\n"
-        for model_name in ["earnings", "expenses"]:
-            model_element = getattr(self,
-                    "_{}_model".format(model_name)).convert_to_xml()
-            period_element.append(model_element)
-        return period_element
-
-    def add_entry(self, **kwargs):
-        value = str(kwargs.pop("value"))
-        category = kwargs.get("category")
-        name = kwargs["name"]
-        date = kwargs.get("date")
-        if value.startswith("-"):
-            self._expenses_model.add_entry(
-                    BaseEntry(name, value, date), category=category)
-        else:
-            self._earnings_model.add_entry(
-                    BaseEntry(name, value, date), category=category)
 
 
 class PeriodException(Exception):
