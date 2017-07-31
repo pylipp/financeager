2d8f7061102e68e80c2621252fab01f7b012eee0
pylipp <philipp.metzner@tum.de>
2017-08-01 00:58:55 +0200

    Vim-style support for navigating the TUI.

--- financeager/tui.py
+++ financeager/tui.py
@@ -26,6 +26,37 @@ class FocusableText(urwid.WidgetWrap):
         return key
 
 
+class VimColumns(urwid.Columns):
+    """Columns subclass supporting horizontal navigation via h and l key as
+    well as scrolling (page up/down) via Ctrl-u/Ctrl-d."""
+
+    def keypress(self, size, key):
+        # check for contents[self.focus_position]._tree parent/child position
+        if key in ['left', 'h']:
+            self.focus_position = 0 if self.focus_position == 1 else 1
+            return
+        elif key in ['right', 'l']:
+            self.focus_position = 1 if self.focus_position == 0 else 0
+            return
+        elif key == "ctrl d":
+            return super().keypress(size, "page down")
+        elif key == "ctrl u":
+            return super().keypress(size, "page up")
+        return super().keypress(size, key)
+
+
+class VimTreeBox(TreeBox):
+    """TreeBox subclass supporting vertical navigation via k and l key."""
+
+    def keypress(self, size, key):
+        w, focuspos = self.get_focus()
+        if key == 'j':
+            return super().keypress(size, 'down')
+        elif key == 'k':
+            return super().keypress(size, 'up')
+        return super().keypress(size, key)
+
+
 def unhandled_input(key):
     """Function to handle input keys at the highest level (MainLoop). Used to
     quit the TUI."""
@@ -51,6 +82,7 @@ def build_tree_from_model(model):
             icon_focussed_at='focus'
             )
 
+
 def run(response):
     """The data from the response (dict with key 'elements' containing list of
     elements in the period database) is sorted into expenses and earnings.
@@ -75,9 +107,9 @@ def run(response):
     model_earnings = Model.from_tinydb(earnings, "Earnings")
     model_expenses = Model.from_tinydb(expenses, "Expenses")
 
-    columns = urwid.Columns([
-            TreeBox(build_tree_from_model(model_earnings)),
-            TreeBox(build_tree_from_model(model_expenses))
+    columns = VimColumns([
+            VimTreeBox(build_tree_from_model(model_earnings)),
+            VimTreeBox(build_tree_from_model(model_expenses))
             ])
     root_widget = urwid.AttrMap(columns, 'body')
     footer = urwid.AttrMap(urwid.Text('Q to quit'), 'focus')
@@ -85,7 +117,7 @@ def run(response):
             ('focus', 'light gray', 'dark blue', 'standout')
             ]
     urwid.MainLoop(
-            urwid.Frame(root_widget, footer=footer), 
+            urwid.Frame(root_widget, footer=footer),
             palette,
             unhandled_input=unhandled_input
             ).run()
