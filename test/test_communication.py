27f169afaebfc635a42678fb350845a075772459
pylipp <philipp.metzner@tum.de>
2017-11-07 14:03:51 +0100

    More communication unittests.

--- test/test_communication.py
+++ test/test_communication.py
@@ -11,17 +11,26 @@ def suite():
     tests = [
             'test_rm',
             'test_get',
-            'test_erroneous_get'
+            'test_erroneous_get',
+            'test_update',
+            'test_print',
             ]
     suite.addTest(unittest.TestSuite(map(CommunicationTestCase, tests)))
+    tests = [
+            'test_rm',
+            'test_get',
+            'test_update',
+            ]
+    suite.addTest(unittest.TestSuite(map(RecurrentEntryCommunicationTestCase, tests)))
     return suite
 
-class CommunicationTestCase(unittest.TestCase):
+class CommunicationTestFixture(unittest.TestCase):
     def run_command(self, args):
         cl_kwargs = _parse_command(args=args.split())
         command = cl_kwargs.pop("command")
         return communication.run(self.proxy, command, **cl_kwargs)
 
+class CommunicationTestCase(CommunicationTestFixture):
     def setUp(self):
         self.proxy = LocalServer(storage=MemoryStorage)
         response = self.run_command("add pants -99 -c clothes")
@@ -54,25 +63,43 @@ Value   : -99.0
 Date    : {}
 Category: Clothes""".format(date.today().strftime("%m-%d")))
 
-class RecurrentEntryCommunicationTestCase(unittest.TestCase):
+    def test_print(self):
+        response = self.run_command("print")
+        self.assertNotIn(communication.ERROR_MESSAGE.format("print", ""),
+                response)
+
+class RecurrentEntryCommunicationTestCase(CommunicationTestFixture):
     def setUp(self):
-        self.period = 0
         self.proxy = LocalServer(storage=MemoryStorage)
-        communication.run(self.proxy, "add", name="retirement money", value=567,
-                period=self.period, category="income", frequency="monthly",
-                start="01-01", table_name="recurrent")
+        response = self.run_command(
+                "add retirement 567 -c income -f monthly -s 01-01 -t recurrent")
+        self.assertEqual(response, "")
+
+    def test_rm(self):
+        response = self.run_command("rm 1 -t recurrent")
+        self.assertEqual(response, "")
 
     def test_get(self):
-        response = communication.run(self.proxy, "get", eid=1,
-                period=self.period, table_name="recurrent")
+        response = self.run_command("get 1 -t recurrent")
         self.assertEqual(response, """\
-Name     : Retirement Money
+Name     : Retirement
 Value    : 567.0
 Frequency: Monthly
 Start    : 01-01
 End      : 12-31
 Category : Income""")
 
+    def test_update(self):
+        response = self.run_command("update 1 -f bimonthly -c n.a. -t recurrent")
+        self.assertEqual(response, "")
+        response = self.run_command("get 1 -t recurrent")
+        self.assertEqual(response, """\
+Name     : Retirement
+Value    : 567.0
+Frequency: Bimonthly
+Start    : 01-01
+End      : 12-31
+Category : N.a.""")
 
 if __name__ == '__main__':
     unittest.main()
