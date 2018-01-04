7e4e2a436543b7c59fe93ca556f30d808c14623f
pylipp <philipp.metzner@tum.de>
2018-01-04 01:18:00 +0100

    Rework test.suites script for running test modules.
    - fix naming/indentation
    - record failures, exit error code 1 if any

--- test/suites.py
+++ test/suites.py
@@ -1,26 +1,30 @@
-# Script to conveniently run all test suites
-#   cd financeager/
-#   python -m src.test.suites
+"""Suite for running all unittests."""
 import unittest
+import sys
 
 modules = [
-        'test_entries',
-        'test_model',
-        'test_period',
-        'test_server',
-        'test_webservice',
-        'test_config',
-        'test_offline',
-        'test_communication'
-        ]
+    'test_entries',
+    'test_model',
+    'test_period',
+    'test_server',
+    'test_webservice',
+    'test_config',
+    'test_offline',
+    'test_communication',
+]
 
-testRunner = unittest.TextTestRunner(verbosity=2)
+test_runner = unittest.TextTestRunner(verbosity=2)
 
 # import modules, find suite via suite() method and run it
+successful = True
 for module in modules:
     exec('from . import {}'.format(module))
     try:
         suite = eval('{}.suite()'.format(module))
-        testRunner.run(suite)
+        result = test_runner.run(suite)
+        successful = successful and result.wasSuccessful()
     except (AttributeError) as e:
         print("{}: {}".format(module, e))
+
+# return error code
+sys.exit(0 if successful else 1)
