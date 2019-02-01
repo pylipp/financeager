351ed3cc8d7bc7248ed83a1b071e69bb5dc1854f
pylipp <philipp.metzner@tum.de>
2019-02-01 13:30:52 +0100

    Add WSGI configuration example

---
+++ examples/wsgi.py
@@ -0,0 +1,20 @@
+# This is an examplatory WSGI configuration file. I use it to run financeager on
+# pythonanywhere.
+# It is assumed that the financeager project is cloned to the home directory and
+# all requirements are installed.
+
+import sys
+import os
+
+from financeager.fflask import create_app
+
+path = os.path.expanduser('~/financeager/financeager/fflask.py')
+if path not in sys.path:
+    sys.path.append(path)
+
+# the 'application' object will be used by the WSGI server
+application = create_app(
+    data_dir=os.path.expanduser('~/.local/share/financeager'),
+    config={
+        "DEBUG": True
+    })
