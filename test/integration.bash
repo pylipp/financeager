4241eef329fefd63828b71dc476ca123e2485479
pylipp <philipp.metzner@tum.de>
2022-01-03 19:41:15 +0100

    Update bash integration test

--- test/integration.bash
+++ test/integration.bash
@@ -3,7 +3,7 @@
 CONFIG_FILEPATH=$(mktemp -t financeager-config-XXXXX)
 
 # Command should succeed with exit code zero
-financeager list -C "$CONFIG_FILEPATH" || exit 1
+fina list -C "$CONFIG_FILEPATH" || exit 1
 
 # Command should fail with non-zero exit code
-financeager list -C "$CONFIG_FILEPATH" --month 13 && exit 1 || exit 0
+fina list -C "$CONFIG_FILEPATH" --month 13 && exit 1 || exit 0
