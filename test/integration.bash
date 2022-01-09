1901ec2d24e1cd4510b1c66ba6fa3182386394d6
pylipp <philipp.metzner@tum.de>
2022-01-09 20:02:58 +0100

    Remove redundant test script
    See #82

--- test/integration.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CONFIG_FILEPATH=$(mktemp -t financeager-config-XXXXX)
-
-# Command should succeed with exit code zero
-fina list -C "$CONFIG_FILEPATH" || exit 1
-
-# Command should fail with non-zero exit code
-fina list -C "$CONFIG_FILEPATH" --month 13 && exit 1 || exit 0
